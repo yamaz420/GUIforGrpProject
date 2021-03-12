97504a3f3bacc6cd9c539318abebef2d3f3068ae
ErikYamaz <62702910+yamaz420@users.noreply.github.com>
2021-03-12 23:18:37 +0100

    Add files via upload

---
+++ SwingGUInov2020/src/swingguinov2020/SwingGUInov2020.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingguinov2020;
+
+/**
+ *
+ * @author Erik
+ */
+public class SwingGUInov2020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+    
+}
